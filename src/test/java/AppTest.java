339af3480a07125f6725abf09dd7fdf521493b59
Daimon21 <testzadan@gmail.com>
2022-02-09 17:47:35 +0200

    add test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,23 @@
-public class AppTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class AppTest {
+//    driver = new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        driver.get("https://www.google.ru/");
+//    JavascriptExecutor jse = (JavascriptExecutor)driver;
+//
+//        driver.findElement(By.name("q")).click();
+//        driver.findElement(By.name("q")).sendKeys("чтото");
+//        driver.findElement(By.name("btnK")).click();
+//        jse.executeScript("window.scrollBy(0,350)");
+//    List<WebElement> some = driver.findElements(By.cssSelector("div.AJLUJb"));
+//
+//    //some = new ArrayList<WebElement>();
+//        System.out.println(some + "yfpdfybt tktvtynjd?");
 }
+//дописать на гугл
--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,18 +1,34 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Login {
     WebDriver driver;
 
-//на гугл забабахать скролл + выборка елементов
+    @BeforeTest
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+
+    }
+
+
     @Test
     public void validLogin(){
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
         driver.get("https://github.com/login");
 
         driver.findElement(By.name("login")).click();
@@ -23,11 +39,22 @@ public class Login {
 
         driver.findElement(By.name("commit")).click();
 
+        Assert.assertTrue(driver.findElement(By.cssSelector("fdfs")).isDisplayed(), "blabla");
+
+    }
+
+//
+//    private boolean isЧтотодисплейд(){
+//        try {
+//            return driver.findElement(By.cssSelector()).isDisplayed();
+//        }catch (Exception e){
+//            return false;
+//        }
+//    }
+
+
+    @AfterTest
+    public void tearDown(){
         driver.quit();
-
-
-
-
-
     }
 }
