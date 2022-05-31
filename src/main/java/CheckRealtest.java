5e376893721de0229ee915142d7c3e2aadd0e544
Daimon2112 <testzadan@gmail.com>
2022-06-01 00:51:18 +0300

    Add login

--- src/main/java/CheckRealtest.java
+++ src/main/java/CheckRealtest.java
@@ -1,17 +1,32 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.asserts.Assertion;
 
 import java.util.concurrent.TimeUnit;
 
 public class CheckRealtest {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chromedriver", "src/main/resources/chromedriver");
-
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
 
 
-        driver.get("");
+        driver.get("https://admin.trycents.dev/login");
+
+        driver.findElement(By.xpath("//input[@type='text']")).click();
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("blabla");
+
+        driver.findElement(By.xpath("//input[@type='password']")).click();
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("blabla");
+
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='label']")).isDisplayed());
+
+
 
     }
 }
