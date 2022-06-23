6cd8c949fec74660bc6e3a8fb92e9bf8c7651acb
Daimon21 <testzadan@gmail.com>
2022-06-23 16:44:21 +0300

    add newbranch

--- src/main/java/javalearn/ImplementInterface.java
+++ src/main/java/javalearn/ImplementInterface.java
@@ -112,12 +112,9 @@ public class ImplementInterface{
 //        }
 //        return map;
 //    }
+12312312312312312
+    12312312312312
+    3123123123123
 
 
-    dsfsdfsdf
-    dsfsdfsdfsd
-
-    public dsfsdfsdf getDsfsdfsdfsd() {
-        sdf
-    }
 }
